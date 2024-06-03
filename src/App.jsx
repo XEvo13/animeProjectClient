10de935a2efabb8ed1d138d75f98a04e668ac133
Francisco Soares <franciscosoares13@hotmail.com>
2024-06-03 15:04:01 +0100

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import axios from "axios";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import AnimeList from './components/AnimeListing'
 import AnimeDetail from './components/AnimeDetail';
+import SignupPage from './pages/SignupPage';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Router>
+    
     <Routes>
       <Route path="/" element={<AnimeList />} />
       <Route path="/anime/:id" element={<AnimeDetail />} />
+      <Route path="/signup" element={ <SignupPage /> } />
     </Routes>
-  </Router>
+  
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css'
 import { AuthProviderWrapper } from './context/auth.context.jsx';
+
 //import { root } from 'postcss';
 
 
